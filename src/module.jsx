f8aca5856ca43a149cc162b628b356d979e1ec58
Михаил Маковчук <krasnodar2312@mail.ru>
2023-02-28 01:55:25 +0300

    не разобрался с scss. жалуется на лоадер

---
+++ src/module.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import buka from "./img/buka.jpg";
+
+const Module = () => (
+  <>
+    <h1>HELLO REACT</h1>
+    <img src={buka} alt="#" />
+  </>
+);
+
+export default Module;
